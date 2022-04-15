99d5c3008d6c2baee65787b89d78e44dc64c4dcd
Amit <amitarazi12345@gmail.com>
2022-04-16 00:00:04 +0300

    deleted delete demos function

--- src/routers/users.js
+++ src/routers/users.js
@@ -2,8 +2,6 @@ const express = require("express");
 const router = new express.Router();
 const signin = require("../shared/users/signin");
 const signup = require("../shared/users/signup");
-const Substitute = require("../Models/substitute");
-const School = require("../Models/school");
 
 router.post("/sub", (req, res) => {
   signup(req, res, "sub");
